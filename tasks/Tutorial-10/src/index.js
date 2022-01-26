bf72cc1d3c7a953724ea854db5a3f3e0f0b5baaf
s21149@pjwstk.edu.pl <s21149@pjwstk.edu.pl>
2022-01-26 16:50:26 +0100

    Task 10 corrected and finished

--- tasks/Tutorial-10/src/index.js
+++ tasks/Tutorial-10/src/index.js
@@ -2,35 +2,38 @@
 const { Celsius, Kelvin, Fahrenheit } = require("./modules/tempConv");
 const { Kilometers, Miles } = require("./modules/distConv");
 
-const celsiusValue = document.getElementById("celsius");
-const fahrenheitValue = document.getElementById("fahrenheit");
-const kelvinValue = document.getElementById("kelvin");
+const celsius = document.getElementById("celsius");
+const fahrenheit = document.getElementById("fahrenheit");
+const kelvin = document.getElementById("kelvin");
 
-const mileValue = document.getElementById("miles");
-const kilometerValue = document.getElementById("kilometers");
+const mile = document.getElementById("miles");
+const kilometer = document.getElementById("kilometers");
 
-const tempForm = document.getElementById("tempForm");
-const distForm = document.getElementById("distForm");
-
-tempForm.addEventListener("submit", (event) => {
-  if (celsiusValue.value !== 0) {
-    fahrenheitValue.value = Celsius.convertToFahrenheit(celsiusValue.value);
-    kelvinValue.value = Celsius.convertToKelvin(celsiusValue.value);
-  } else if (fahrenheitValue.value !== 0) {
-    celsiusValue.value = Fahrenheit.convertToCelsius(fahrenheitValue.value);
-    kelvinValue.value = Fahrenheit.convertToKelvin(fahrenheitValue.value);
-  } else if (kelvinValue.value !== 0) {
-    celsiusValue.value = Kelvin.convertToCelsius(kelvinValue.value);
-    fahrenheitValue.value = Kelvin.convertToFahrenheit(kelvinValue.value);
-  }
+celsius.addEventListener('change', event => {
+  fahrenheit.value = Celsius.convertToFahrenheit(celsius.value);
+  kelvin.value = Celsius.convertToKelvin(celsius.value);
   event.preventDefault();
-})
+});
 
-distForm.addEventListener("submit", (event) => {
-  if (kilometerValue.value !== 0) {
-    mileValue.value = Kilometers.convertToMiles(kilometerValue.value);
-  } else if (mileValue.value !== 0) {
-    kilometerValue.value = Miles.convertToKilometers(mileValue.value);
-  }
+fahrenheit.addEventListener('change', event => {
+  celsius.value = Fahrenheit.convertToFahrenheit(fahrenheit.value);
+  kelvin.value = Fahrenheit.convertToKelvin(fahrenheit.value);
   event.preventDefault();
-})
+});
+
+kelvin.addEventListener('change', event => {
+  celsius.value = Kelvin.convertToCelsius(kelvin.value);
+  fahrenheit.value = Kelvin.convertToFahrenheit(kelvin.value);
+  event.preventDefault();
+});
+
+mile.addEventListener('change', event => {
+  kilometer.value = Miles.convertToKilometers(mile.value);
+  event.preventDefault();
+});
+
+kilometer.addEventListener('change', event => {
+  mile.value = Kilometers.convertToMiles(kilometer.value);
+  event.preventDefault();
+});
+
